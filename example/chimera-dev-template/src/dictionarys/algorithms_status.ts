c914e99721f35c8d2102c121e5fb7d5f0b32c9fe
yushicheng <1542874601@qq.com>
2022-04-28 23:40:55 +0800

    temp:临时节点标记 星期四 晚上 11:40:55 四月 28日 2022 删除了 example/chimera-dev-template/.babelrc.js 删除了 example/chimera-dev-template/.eslintrc.js 删除了 example/chimera-dev-template/.gitignore 删除了 example/chimera-dev-template/.npmrc 删除了 example/chimera-dev-template/.stylelintrc.json 删除了 example/chimera-dev-template/README.md 删除了 example/chimera-dev-template/chimera.config.js 删除了 example/chimera-dev-template/configs/client.template.js 删除了 example/chimera-dev-template/configs/proxy_middleware.js 删除了 example/chimera-dev-template/configs/server.template.js 删除了 example/chimera-dev-template/package.json 删除了 example/chimera-dev-template/postcss.config.js 删除了 example/chimera-dev-template/public/favicon.ico 删除了 example/chimera-dev-template/public/test.html 删除了 example/chimera-dev-template/server/dev.js 删除了 example/chimera-dev-template/server/devRender.js 删除了 example/chimera-dev-template/server/prod.js 删除了 example/chimera-dev-template/server/prodRender.js 删除了 example/chimera-dev-template/src/components/ShowResponsive/index.js 删除了 example/chimera-dev-template/src/components/ShowResponsive/style.scss 删除了 example/chimera-dev-template/src/dictionarys/algorithms_status.ts 删除了 example/chimera-dev-template/src/entry.js 删除了 example/chimera-dev-template/src/hooks/useResponsive.js 删除了 example/chimera-dev-template/src/index.html 删除了 example/chimera-dev-template/src/init18n.js 删除了 example/chimera-dev-template/src/locales/en.js 删除了 example/chimera-dev-template/src/locales/zh.js 删除了 example/chimera-dev-template/src/pages/IndexPage/index.js 删除了 example/chimera-dev-template/src/pages/IndexPage/style.less 删除了 example/chimera-dev-template/src/pages/IndexPage/v2-5c21be97780f6be9aeb5bb2d01ae2470_r.jpg

--- example/chimera-dev-template/src/dictionarys/algorithms_status.ts
+++
@@ -1,9 +0,0 @@
-
-enum algorithms_status {
-  RunningOnOribit,
-  UploadNow,
-  TestNow,
-  TestRunning
-}
-
-export { algorithms_status };
--- example/chimera-dev-template/src/locales/en.js
+++
@@ -1,8 +0,0 @@
-
-
-export default {
-  "多语言切换测试":"test change language",
-  "当前定时为":"current timming is",
-  "暂停":"zanting",
-  "播放":"bofang"
-};
