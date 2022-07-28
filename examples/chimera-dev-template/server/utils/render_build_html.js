d7324ce717d0fc43e1796dfb9586a525353723bd
yushicheng <1542874601@qq.com>
2022-07-29 00:55:50 +0800

    temp:临时节点标记 星期五 凌晨 12:55:50 七月 29日 2022 修改了 examples/chimera-dev-template/server/utils/render_build_html.js

--- examples/chimera-dev-template/server/utils/render_build_html.js
+++ examples/chimera-dev-template/server/utils/render_build_html.js
@@ -9,8 +9,8 @@ module.exports = function render_html() {
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="shortcut icon" href="/public/favicon.ico">
-      <script defer src="/main.js"></script>
-      <link href="/main.css" rel="stylesheet">
+      <script defer src="/assets/main.js"></script>
+      <link href="/assets/main.css" rel="stylesheet">
     </head>
     <body>
       <div id="root"></div>
