2140f3258c5f7ef8fbcb4c6b6fcf30988f76fd80
yushicheng <1542874601@qq.com>
2022-08-01 23:32:40 +0800

    temp:临时节点标记 星期一 晚上 11:32:40 八月 1日 2022 修改了 examples/chimera-dev-template/package.json 修改了 examples/chimera-dev-template/server/app.js

--- examples/chimera-dev-template/server/app.js
+++ examples/chimera-dev-template/server/app.js
@@ -5,19 +5,23 @@ const static = require("koa-static");
 const cookieParser = require("koa-cookie");
 
 const render = require("./middlewares/render");
-const proxy_list = require("../configs/proxy_list");
 const index_page = require("./routers/index_page");
 
 const app = new Koa();
 app.use(static("./", { index: false }));
 app.use(static("./", { index: false }));
 
-Object.keys(proxy_list).map((current_proxy_path) => {
-  app.use(proxy(current_proxy_path, proxy_list[current_proxy_path]))
-});
+/** 本地开发模式需要代理 **/
+if (process.env.CHIMERA_RUNTIME === "local") {
+  const proxy_list = require("../configs/proxy_list");
+  Object.keys(proxy_list).map((current_proxy_path) => {
+    app.use(proxy(current_proxy_path, proxy_list[current_proxy_path]))
+  });
+};
 
 app.use(cookieParser.default());
 app.use(render);
 app.use(index_page);
 app.listen(8090, () => console.log("server is runing 8090"));
+
 
