8116096437f6a30d0c805e719d4478e0d674d9db
yushicheng <1542874601@qq.com>
2022-09-26 19:18:50 +0800

    temp:临时节点标记 星期一 晚上 7:18:50 九月 26日 2022 修改了 examples/chimera-dev-template/package.json 修改了 examples/chimera-dev-template/postcss.config.js 修改了 examples/chimera-dev-template/server/middlewares/render.js

--- examples/chimera-dev-template/server/middlewares/render.js
+++ examples/chimera-dev-template/server/middlewares/render.js
@@ -10,31 +10,26 @@ module.exports = async () => {
   const render_template = await promisify(fs.readFile)(html_template_path, "utf-8");
   return async (context, next) => {
     try {
-      const { API_TOKEN } = context.cookie = {};
-      console.log("当前身份令牌==>", API_TOKEN);
+      // const { API_TOKEN } = context.cookie = {};
+      // console.log("当前身份令牌==>", API_TOKEN);
       const return_values = await next();
-      if (!return_values) {
-        context.response.status = 301;
-        await context.redirect("/");
-      } else {
-        const { language = "zh", seo_option = {}, initial_value } = return_values;
-        const render_html = await server_render({
-          language,
-          dev_inject: {},
-          basename: "/",
-          initial_value,
-          location: context.path,
-          html_template: render_template,
-          seo_option: {
-            ...seo_option,
-            title: seo_option.title || "免版权视频素材下载",
-            keywords: seo_option.keywords || "免版权, 视频, 素材, 下载",
-            description: seo_option.description || "免版权高清视频素材下载",
-          }
-        });
-        context.response.status = 200;
-        context.response.body = render_html;
-      };
+      const { language = "zh", seo_option = {}, initial_value } = return_values || {};
+      const render_html = await server_render({
+        language,
+        dev_inject: {},
+        basename: "/",
+        initial_value,
+        location: context.path,
+        html_template: render_template,
+        seo_option: {
+          ...seo_option,
+          title: seo_option.title || "免版权视频素材下载",
+          keywords: seo_option.keywords || "免版权, 视频, 素材, 下载",
+          description: seo_option.description || "免版权高清视频素材下载",
+        }
+      });
+      context.response.status = 200;
+      context.response.body = render_html;
     } catch (error) {
       console.log(error);
       context.response.body = `<pre>${error.message}</pre>`;
