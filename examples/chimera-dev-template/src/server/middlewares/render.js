3025d2a934171ee5e5dc162556416048dbebd8c1
frankleo <154284601@qq.com>
2023-02-24 09:25:26 +0800

    temp:临时节点标记 星期五 上午 9:25:26 二月 24日 2023 修改了 examples/chimera-dev-template/src/server/middlewares/render.js

--- examples/chimera-dev-template/src/server/middlewares/render.js
+++ examples/chimera-dev-template/src/server/middlewares/render.js
@@ -2,28 +2,39 @@
 import { server_render } from "./render.template";
 
 export async function runder_method(context, next) {
-  try {
-    // const { API_TOKEN } = context.cookie = {};
-    // console.log("当前身份令牌==>", API_TOKEN);
-    const return_values = await next();
-    const { language = "zh", seo_option = {}, initial_value } = return_values || {};
-    const render_html = await server_render({
-      language,
-      dev_inject: {},
-      basename: "/",
-      initial_value,
-      location: context.path,
-      seo_option: {
-        ...seo_option,
-        title: seo_option.title || "chimera-project",
-        keywords: seo_option.keywords || "cyber,chimera,project,",
-        description: seo_option.description || "this is chimera-project",
-      }
-    });
-    context.response.status = 200;
-    context.response.body = render_html;
-  } catch (error) {
-    console.log(error);
-    context.response.body = `<pre>${error.message}</pre>`;
+  const { API_TOKEN } = context.cookie = {};
+  console.log("当前身份令牌==>", API_TOKEN);
+  if (context.path.match("api")) {
+    try {
+      const return_values = await next();
+      context.response.status = 200;
+      context.response.body = { code: 0, data: return_values, message: "ok" };
+    } catch (error) {
+      context.response.status = 200;
+      context.response.body = { code: 0, data: null, message: JSON.stringify(error) };;
+    };
+  } else {
+    try {
+      const return_values = await next();
+      const { language = "zh", seo_option = {}, initial_value } = return_values || {};
+      const render_html = await server_render({
+        language,
+        dev_inject: {},
+        basename: "/",
+        initial_value,
+        location: context.path,
+        seo_option: {
+          ...seo_option,
+          title: seo_option.title || "chimera-project",
+          keywords: seo_option.keywords || "cyber,chimera,project,",
+          description: seo_option.description || "this is chimera-project",
+        }
+      });
+      context.response.status = 200;
+      context.response.body = render_html;
+    } catch (error) {
+      console.log(error);
+      context.response.body = `<pre>${error.message}</pre>`;
+    };
   };
 };
