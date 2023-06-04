5de342f47ab28f913af73e1b6d4a0a8d9f07c664
yushicheng <1542874601@qq.com>
2023-06-04 19:18:33 +0800

    temp:临时节点标记 星期日 晚上 7:18:33 六月 4日 2023 创建了 examples/chimera-dev-template/src/utils/render_content.tsx 修改了 examples/chimera-dev-template/src/routers/index.ssr.tsx

--- examples/chimera-dev-template/src/routers/index.ssr.tsx
+++ examples/chimera-dev-template/src/routers/index.ssr.tsx
@@ -1,39 +1,9 @@
-import path from "path";
-import React from "react";
-import { promisify } from "util";
-import { readFile } from "jsonfile";
-import { renderToString } from "react-dom/server";
-import { StaticRouter } from "react-router-dom/server";
-
-import { Application } from "@/application/application";
-
-const mainfast_filepath = {
-  "local": path.resolve(process.cwd(), "./.temp/manifest.json"),
-  "test": path.resolve(process.cwd(), "./assets/manifest.json"),
-  "prod": path.resolve(process.cwd(), "./assets/manifest.json")
-}[process.env.NODE_ENV];
+import { render_content } from "@/utils/render_content";
 
 export const render_index = async (request, response) => {
-  const mainfast = await promisify(readFile)(mainfast_filepath);
-  const render_content = renderToString(
-    <html>
-      <head>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>cyber-chimera-application</title>
-        <link rel="stylesheet" href={mainfast["main.css"]} />
-      </head>
-      <body>
-        <div id="root">
-          <StaticRouter location={request.url}>
-            <Application />
-          </StaticRouter>
-        </div>
-        <script src={mainfast["vendors.js"]}></script>
-        <script src={mainfast["main.js"]}></script>
-      </body>
-    </html>
-  );
-  response.send(render_content);
+  const render_content_string = await render_content({
+    request_url: request.url,
+    initial_value: {}
+  });
+  response.send(render_content_string);
 };
---
+++ examples/chimera-dev-template/src/utils/render_content.tsx
@@ -0,0 +1,43 @@
+import path from "path";
+import React from "react";
+import { promisify } from "util";
+import { readFile } from "jsonfile";
+import { renderToString } from "react-dom/server";
+import { StaticRouter } from "react-router-dom/server";
+
+import { RenderContextProvider } from "@/application/context/render_context";
+import { Application } from "@/application/application";
+
+const mainfast_filepath = {
+  "local": path.resolve(process.cwd(), "./.temp/manifest.json"),
+  "test": path.resolve(process.cwd(), "./assets/manifest.json"),
+  "prod": path.resolve(process.cwd(), "./assets/manifest.json")
+}[process.env.NODE_ENV];
+
+export async function render_content({ title = "Frank.leo的磁力站", request_url, initial_value }) {
+  const mainfast = await promisify(readFile)(mainfast_filepath);
+  const content_string = renderToString(
+    <html>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{title}</title>
+        <link rel="stylesheet" href={mainfast["main.css"]} />
+        <script dangerouslySetInnerHTML={{ __html: `window.initial_value=${JSON.stringify(initial_value, null, "")}` }}></script>
+      </head>
+      <body>
+        <div id="root">
+          <RenderContextProvider initial_value={initial_value}>
+            <StaticRouter location={request_url}>
+              <Application />
+            </StaticRouter>
+          </RenderContextProvider>
+        </div>
+        <script src={mainfast["vendors.js"]}></script>
+        <script src={mainfast["main.js"]}></script>
+      </body>
+    </html>
+  );
+  return content_string;
+};
