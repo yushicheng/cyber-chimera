2109f9d3416b67f9b31543ac31be99d38fd4a224
yushicheng <1542874601@qq.com>
2023-06-07 00:09:12 +0800

    temp:临时节点标记 星期三 凌晨 12:09:12 六月 7日 2023 修改了 examples/chimera-dev-template/src/utils/render_content.tsx

--- examples/chimera-dev-template/src/utils/render_content.tsx
+++ examples/chimera-dev-template/src/utils/render_content.tsx
@@ -10,9 +10,7 @@ import { RenderContextProvider } from "@/application/context/render_context";
 import { Application } from "@/application/application";
 
 let mainfast = {};
-let style_content = "";
 
-const application_dirname = path.resolve(path.dirname(__filename), "./application/");
 const mainfast_filepath = {
   "local": path.resolve(process.cwd(), "./.temp/manifest.json"),
   "test": path.resolve(process.cwd(), "./assets/manifest.json"),
@@ -21,8 +19,6 @@ const mainfast_filepath = {
 
 export async function initial_render_content() {
   mainfast = await promisify(readFile)(mainfast_filepath);
-  const style_filename = mainfast["main.css"];
-  style_content = await promisify(fs.readFile)(path.resolve(application_dirname, style_filename), "utf-8");
 };
 
 
@@ -35,7 +31,8 @@ export async function render_content({ title = "默认标题", request_url, init
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
-        <style>{style_content}</style>
+        <link href="favicon.ico" />
+        <link rel="stylesheet" href={`/${mainfast["main.css"]}`} />
         <script dangerouslySetInnerHTML={{ __html: `window.initial_value=${JSON.stringify(initial_value, null, "")}` }}></script>
       </head>
       <body>
